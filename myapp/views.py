c9a0b55148dff001243f23a5d4e9272299734fdd
sreekanthreddyv <root@localhost.localdomain>
2016-12-23 23:48:40 +0530

    hacker

--- myapp/views.py
+++ myapp/views.py
@@ -2,59 +2,63 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import DreamReal
 
+
 # Create your views here.
 
 # Sending Parameters to Views
 def viewArticle(request, articleID):
-	text = "Displaying the article: %s"%articleID
-	return HttpResponse(text)
+    text = "Displaying the article: %s" % articleID
+    return HttpResponse(text)
+
 
 def viewArticles(request, month, year):
-	text = "Displaying the article: {}-{}".format(month, year)
-	return HttpResponse(text)
+    text = "Displaying the article: {}-{}".format(month, year)
+    return HttpResponse(text)
+
 
 def disdate(request):
-	from datetime import date
-	day = date.today()
-	return render(request, "myapp/hello.html", {'today': day})
+    from datetime import date
+    day = date.today()
+    return render(request, "myapp/hello.html", {'today': day})
+
 
 def crudops(request):
-	# creating an Entry
+    # creating an Entry
 
-	dreamreal = DreamReal(
-		website = "www.n1tr0g3n.com", mail = "HackerOnline@net.com",
-		name = "Aetos", phonenumber = "1918179940"
-		)
-	dreamreal.save()
+    dreamreal = DreamReal(
+        website="www.n1tr0g3n.com", mail="HackerOnline@net.com",
+        name="Aetos", phonenumber="1918179940"
+    )
+    dreamreal.save()
 
-	# Real all Entries
-	objects = DreamReal.objects.all()
-	res ='Printing all DreamReal entries in the DB : <br>'
+    # Real all Entries
+    objects = DreamReal.objects.all()
+    res = 'Printing all DreamReal entries in the DB : <br>'
 
-	for elt in objects:
-		res += elt.name + '<br>'
+    for elt in objects:
+        res += elt.name + '<br>'
 
-	# Read specific entry
+    # Read specific entry
 
-	sorex = DreamReal.objects.get(name = 'aetos')
-	res += 'Printing One entry <br>'
-   	res += sorex.name
+    sorex = DreamReal.objects.get(name='aetos')
+    res += 'Printing One entry <br>'
+    res += sorex.name
 
-   	# Delete an entry
-   	res += '<br> Deleting an entry <br>'
-   	sorex.delete()
+    # Delete an entry
+    res += '<br> Deleting an entry <br>'
+    sorex.delete()
 
-   	# Update
-   	dreamreal = DreamReal(
-      		website = "www.polo.com", mail = "sorex@polo.com", 
-      		name = "sorex", phonenumber = "002376970"
-   		)
-   
-   	dreamreal.save()
-   	res += 'Updating entry<br>'
+    # Update
+    dreamreal = DreamReal(
+        website="www.polo.com", mail="sorex@polo.com",
+        name="sorex", phonenumber="002376970"
+    )
 
-   	dreamreal = DreamReal.objects.get(name = 'sorex')
-   	dreamreal.name = 'thierry'
-   	dreamreal.save()
+    dreamreal.save()
+    res += 'Updating entry<br>'
 
-   	return HttpResponse(res)
+    dreamreal = DreamReal.objects.get(name='sorex')
+    dreamreal.name = 'thierry'
+    dreamreal.save()
+
+    return HttpResponse(res)
